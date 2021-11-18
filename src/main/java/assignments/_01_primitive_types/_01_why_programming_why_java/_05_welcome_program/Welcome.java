aec3ecc533ce4f28ce0e874fffa1923efdbddaee
corock <hoongineer@gmail.com>
2021-11-18 23:50:35 +0900

    Add welcome program

---
+++ src/main/java/assignments/_01_primitive_types/_01_why_programming_why_java/_05_welcome_program/Welcome.java
@@ -0,0 +1,12 @@
+package assignments._01_primitive_types._01_why_programming_why_java._05_welcome_program;
+
+public class Welcome {
+    public static void main(String[] args) {
+        // Your code will go here
+        // These lines are comments
+        // Java ignores anything that comes after
+        // two forward slashes
+        // You can delete the comments or leave them in
+        // Java ignores them anyway!
+    }
+}
