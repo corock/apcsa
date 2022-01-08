bdb7f128965719c453acfd055433c2b070814558
corock <hoongineer@gmail.com>
2022-01-08 23:10:05 +0900

    Add heating up

---
+++ src/main/java/assignments/_01_primitive_types/_01_why_programming_why_java/_08_heating_up/MakingPopcorn.java
@@ -0,0 +1,7 @@
+package assignments._01_primitive_types._01_why_programming_why_java._08_heating_up;
+
+public class MakingPopcorn {
+    public static void main(String[] args) {
+        // Start here!
+    }
+}
