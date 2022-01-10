f98fb33e3094ae4fe4eb42215b61bec412978d4f
corock <hoongineer@gmail.com>
2022-01-10 23:34:57 +0900

    Add a personal timeline of events

---
+++ src/main/java/assignments/_01_primitive_types/_01_why_programming_why_java/_09_personal_timeline/Timeline.java
@@ -0,0 +1,7 @@
+package assignments._01_primitive_types._01_why_programming_why_java._09_personal_timeline;
+
+public class Timeline {
+    public static void main(String[] args) {
+        // Start here!
+    }
+}
