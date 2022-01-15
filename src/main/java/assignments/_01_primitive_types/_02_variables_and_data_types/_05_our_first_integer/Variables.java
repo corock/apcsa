fe2be20a105a7c884b7b9c1d0dffdcb4e375eccb
corock <hoongineer@gmail.com>
2022-01-15 23:59:02 +0900

    Add our first integer

---
+++ src/main/java/assignments/_01_primitive_types/_02_variables_and_data_types/_05_our_first_integer/Variables.java
@@ -0,0 +1,7 @@
+package assignments._01_primitive_types._02_variables_and_data_types._05_our_first_integer;
+
+public class Variables {
+    public static void main(String[] args) {
+        // Start here!
+    }
+}
