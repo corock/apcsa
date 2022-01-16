d6456891575ae2c5372276c6786b7dbf2101a763
corock <hoongineer@gmail.com>
2022-01-16 23:29:02 +0900

    Write a program that declares an int named year and set it equal to the current year

--- src/main/java/assignments/_01_primitive_types/_02_variables_and_data_types/_05_our_first_integer/Variables.java
+++ src/main/java/assignments/_01_primitive_types/_02_variables_and_data_types/_05_our_first_integer/Variables.java
@@ -2,6 +2,7 @@ package assignments._01_primitive_types._02_variables_and_data_types._05_our_fir
 
 public class Variables {
     public static void main(String[] args) {
-        // Start here!
+        int year = 2022;
+        System.out.println("The current year is " + year);
     }
 }
