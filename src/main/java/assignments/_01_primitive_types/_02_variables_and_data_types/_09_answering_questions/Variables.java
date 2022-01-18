76c921db494881794cf9b0ff8c3ebe673cbc9cb4
corock <hoongineer@gmail.com>
2022-01-18 23:51:44 +0900

    Add answering questions by using variables

---
+++ src/main/java/assignments/_01_primitive_types/_02_variables_and_data_types/_09_answering_questions/Variables.java
@@ -0,0 +1,15 @@
+package assignments._01_primitive_types._02_variables_and_data_types._09_answering_questions;
+
+public class Variables {
+    public static void main(String[] args) {
+        String myName =
+        int luckyNumber =
+        double currentTemperature =
+        boolean isStudent =
+
+        System.out.println(myName);
+        System.out.println(luckyNumber);
+        System.out.println(currentTemperature);
+        System.out.println(isStudent);
+    }
+}
