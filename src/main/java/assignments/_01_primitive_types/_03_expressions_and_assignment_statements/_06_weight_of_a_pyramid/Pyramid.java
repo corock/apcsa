45752082399831b11454e4f1fd5a3f7850f790b3
corock <hoongineer@gmail.com>
2022-01-23 22:27:21 +0900

    Add writing a program that estimates how much Kufu’s pyramid in Giza weighs

---
+++ src/main/java/assignments/_01_primitive_types/_03_expressions_and_assignment_statements/_06_weight_of_a_pyramid/Pyramid.java
@@ -0,0 +1,7 @@
+package assignments._01_primitive_types._03_expressions_and_assignment_statements._06_weight_of_a_pyramid;
+
+public class Pyramid {
+    public static void main(String[] args) {
+
+    }
+}
