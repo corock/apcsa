2a9dea25266b62d6b57acee34fc7e2cedc079e3b
corock <hoongineer@gmail.com>
2022-01-29 23:14:04 +0900

    Add sum of two fractions

---
+++ src/main/java/assignments/_01_primitive_types/_03_expressions_and_assignment_statements/_07_add_fractions/AddFractions.java
@@ -0,0 +1,7 @@
+package assignments._01_primitive_types._03_expressions_and_assignment_statements._07_add_fractions;
+
+public class AddFractions {
+    public static void main(String[] args) {
+        // Your code goes here!
+    }
+}
