98984295fb12fa10796b5e169e56e7e175f8d4e4
corock <hoongineer@gmail.com>
2022-01-30 15:40:47 +0900

    Implement sum of two fractions

--- src/main/java/assignments/_01_primitive_types/_03_expressions_and_assignment_statements/_07_add_fractions/AddFractions.java
+++ src/main/java/assignments/_01_primitive_types/_03_expressions_and_assignment_statements/_07_add_fractions/AddFractions.java
@@ -2,6 +2,31 @@ package assignments._01_primitive_types._03_expressions_and_assignment_statement
 
 public class AddFractions {
     public static void main(String[] args) {
-        // Your code goes here!
+        int numeratorOne = 1;
+        int denominatorOne = 4;
+        int numeratorTwo = 4;
+        int denominatorTwo = 6;
+
+        System.out.print("The numerator of the first fraction is " + numeratorOne + "\n");
+        System.out.print("The denominator of the first fraction is " + denominatorOne + "\n");
+        System.out.print("The numerator of the second fraction is " + numeratorTwo + "\n");
+        System.out.print("The denominator of the second fraction is " + denominatorTwo + "\n");
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("The sum of ");
+        sb.append(numeratorOne);
+        sb.append("/");
+        sb.append(denominatorOne);
+        sb.append(" + ");
+        sb.append(numeratorTwo);
+        sb.append("/");
+        sb.append(denominatorTwo);
+        sb.append(" = ");
+        sb.append((numeratorOne * denominatorTwo) + (numeratorTwo * denominatorOne));
+        sb.append("/");
+        sb.append(denominatorOne * denominatorTwo);
+
+        System.out.println(sb);
     }
 }
