b34c3c01eaa974856f1508b5e47a13d9ee3054a6
corock <hoongineer@gmail.com>
2022-01-31 16:29:08 +0900

    Add free fall problems

---
+++ src/main/java/assignments/_01_primitive_types/_03_expressions_and_assignment_statements/_08_freely_falling_bodies/FallingBodies.java
@@ -0,0 +1,7 @@
+package assignments._01_primitive_types._03_expressions_and_assignment_statements._08_freely_falling_bodies;
+
+public class FallingBodies {
+    public static void main(String[] args) {
+        // Start here!
+    }
+}
