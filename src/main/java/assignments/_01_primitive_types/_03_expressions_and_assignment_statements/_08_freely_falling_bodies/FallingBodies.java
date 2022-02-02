a26625951d888222c1d5cdad0db04983b3a2e274
corock <hoongineer@gmail.com>
2022-02-02 22:46:10 +0900

    Solve free fall problems

--- src/main/java/assignments/_01_primitive_types/_03_expressions_and_assignment_statements/_08_freely_falling_bodies/FallingBodies.java
+++ src/main/java/assignments/_01_primitive_types/_03_expressions_and_assignment_statements/_08_freely_falling_bodies/FallingBodies.java
@@ -2,6 +2,9 @@ package assignments._01_primitive_types._03_expressions_and_assignment_statement
 
 public class FallingBodies {
     public static void main(String[] args) {
-        // Start here!
+        final double g = 9.8;
+        double t = 10;
+        System.out.print("The height is " + (0.5 * g * t * t) + "\n");
+        System.out.print("The velocity is " + (g * t));
     }
 }
