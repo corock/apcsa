bbc579f7ea4a5c745903c49c44217b073babae87
corock <hoongineer@gmail.com>
2022-02-03 22:38:27 +0900

    Add Converting the total shift time into seconds

---
+++ src/main/java/assignments/_01_primitive_types/_04_compound_assignment_operators/_05_work_shift/WorkShift.java
@@ -0,0 +1,7 @@
+package assignments._01_primitive_types._04_compound_assignment_operators._05_work_shift;
+
+public class WorkShift {
+    public static void main(String[] args) {
+        // Start here
+    }
+}
