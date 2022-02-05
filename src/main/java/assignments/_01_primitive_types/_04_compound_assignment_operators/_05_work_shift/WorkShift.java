c77c57d82d3be7dcf43615af0ad91f22e7f8b034
corock <hoongineer@gmail.com>
2022-02-05 22:03:58 +0900

    Implement converting the total shift time into seconds

--- src/main/java/assignments/_01_primitive_types/_04_compound_assignment_operators/_05_work_shift/WorkShift.java
+++ src/main/java/assignments/_01_primitive_types/_04_compound_assignment_operators/_05_work_shift/WorkShift.java
@@ -2,6 +2,12 @@ package assignments._01_primitive_types._04_compound_assignment_operators._05_wo
 
 public class WorkShift {
     public static void main(String[] args) {
-        // Start here
+        int hours = 20;
+        int minutes = 42;
+        int seconds = 16;
+
+        minutes += hours * 60;
+        seconds += minutes * 60;
+        System.out.println(seconds);
     }
 }
