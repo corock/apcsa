ee4bef16e3cd366d55aa4cccbb1982c920ee675d
corock <hoongineer@gmail.com>
2022-02-07 00:06:25 +0900

    Update MyAge by using increment and decrement operators

--- src/main/java/assignments/_01_primitive_types/_04_compound_assignment_operators/_06_my_age/MyAge.java
+++ src/main/java/assignments/_01_primitive_types/_04_compound_assignment_operators/_06_my_age/MyAge.java
@@ -2,6 +2,9 @@ package assignments._01_primitive_types._04_compound_assignment_operators._06_my
 
 public class MyAge {
     public static void main(String[] args) {
-        // Start here!
+        int age = 19;
+        System.out.println("My current age is: " + age++);
+        System.out.println("My age next year will be: " + age--);
+        System.out.println("My current age is: " + age);
     }
 }
