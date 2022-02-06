2f2668e0e96e02e757c51cf21c5da5eb06874dc5
corock <hoongineer@gmail.com>
2022-02-06 22:11:49 +0900

    Add producing the output for your current and upcoming age next year, then back to your current age

---
+++ src/main/java/assignments/_01_primitive_types/_04_compound_assignment_operators/_06_my_age/MyAge.java
@@ -0,0 +1,7 @@
+package assignments._01_primitive_types._04_compound_assignment_operators._06_my_age;
+
+public class MyAge {
+    public static void main(String[] args) {
+        // Start here!
+    }
+}
