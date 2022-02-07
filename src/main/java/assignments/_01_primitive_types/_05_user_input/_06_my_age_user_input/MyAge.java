c9563f785c71a7caeb9a917b8def283dea4be079
corock <hoongineer@gmail.com>
2022-02-07 23:59:34 +0900

    Modify MyAge exercise using the Scanner class to take user input

---
+++ src/main/java/assignments/_01_primitive_types/_05_user_input/_06_my_age_user_input/MyAge.java
@@ -0,0 +1,13 @@
+package assignments._01_primitive_types._05_user_input._06_my_age_user_input;
+
+import java.util.Scanner;
+
+// Refer to your code from the previous My Age exercise.
+// Modify it using the Scanner class to take user input instead of hard coding in your age.
+
+public class MyAge {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        // Start here!
+    }
+}
