a847b181b8fe2b95716ae85ab247ac5f8c4cd044
corock <hoongineer@gmail.com>
2022-02-09 23:20:37 +0900

    Add writing a program to help calculate the total cost

---
+++ src/main/java/assignments/_01_primitive_types/_05_user_input/_07_night_out/NightOut.java
@@ -0,0 +1,10 @@
+package assignments._01_primitive_types._05_user_input._07_night_out;
+
+import java.util.*;
+
+public class NightOut {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        // Start here!
+    }
+}
