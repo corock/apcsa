5e87ee828de983e82e88948e80e132e92ae704e2
corock <hoongineer@gmail.com>
2022-02-10 23:37:37 +0900

    Implement writing a program to help calculate the total cost

--- src/main/java/assignments/_01_primitive_types/_05_user_input/_07_night_out/NightOut.java
+++ src/main/java/assignments/_01_primitive_types/_05_user_input/_07_night_out/NightOut.java
@@ -5,6 +5,21 @@ import java.util.*;
 public class NightOut {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        // Start here!
+
+        System.out.println("How much did dinner cost?");
+        double dinner = input.nextDouble();
+
+        System.out.println("How much is mini - golf for one person?");
+        double golf = input.nextDouble();
+
+        System.out.println("How much did dessert cost?");
+        double desert = input.nextDouble();
+
+        double total = dinner + (2 * golf) + desert;
+
+        System.out.println("Dinner: $" + dinner);
+        System.out.println("Mini-Golf: $" + golf);
+        System.out.println("Dessert: $" + desert);
+        System.out.println("Grand Total: $" + total);
     }
 }
