54816ca21c594c9d4c57c89b16742ad4e1a13095
corock <hoongineer@gmail.com>
2022-02-11 22:38:55 +0900

    Add writing a citation generator

---
+++ src/main/java/assignments/_01_primitive_types/_05_user_input/_08_mla_citation/Citation.java
@@ -0,0 +1,9 @@
+package assignments._01_primitive_types._05_user_input._08_mla_citation;
+
+import java.util.Scanner;
+
+public class Citation {
+    public static void main(String[] args) {
+        // Start here!
+    }
+}
