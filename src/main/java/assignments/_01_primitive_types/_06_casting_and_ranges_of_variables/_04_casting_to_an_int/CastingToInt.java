a39182e477d1f520f274b1ebf2ce3f4f0b309b1a
corock <hoongineer@gmail.com>
2022-02-15 22:52:41 +0900

    Add casting to an int

---
+++ src/main/java/assignments/_01_primitive_types/_06_casting_and_ranges_of_variables/_04_casting_to_an_int/CastingToInt.java
@@ -0,0 +1,9 @@
+package assignments._01_primitive_types._06_casting_and_ranges_of_variables._04_casting_to_an_int;
+
+import java.util.Scanner;
+
+public class CastingToInt {
+    public static void main(String[] args) {
+        // Start here!
+    }
+}
