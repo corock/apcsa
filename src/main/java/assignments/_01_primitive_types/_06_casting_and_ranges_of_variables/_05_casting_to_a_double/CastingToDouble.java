7cfb4d98c5e569f186052d4d47a7455e0382cbae
corock <hoongineer@gmail.com>
2022-02-16 22:08:12 +0900

    Add casting to a double

---
+++ src/main/java/assignments/_01_primitive_types/_06_casting_and_ranges_of_variables/_05_casting_to_a_double/CastingToDouble.java
@@ -0,0 +1,9 @@
+package assignments._01_primitive_types._06_casting_and_ranges_of_variables._05_casting_to_a_double;
+
+import java.util.Scanner;
+
+public class CastingToDouble {
+    public static void main(String[] args) {
+        // Start here!
+    }
+}
