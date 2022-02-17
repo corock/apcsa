9fc06fb02fcbdbe1bd2e27f4cc0cd41341949aee
corock <hoongineer@gmail.com>
2022-02-17 23:19:50 +0900

    Write a program that includes casting to a double

--- src/main/java/assignments/_01_primitive_types/_06_casting_and_ranges_of_variables/_05_casting_to_a_double/CastingToDouble.java
+++ src/main/java/assignments/_01_primitive_types/_06_casting_and_ranges_of_variables/_05_casting_to_a_double/CastingToDouble.java
@@ -4,6 +4,11 @@ import java.util.Scanner;
 
 public class CastingToDouble {
     public static void main(String[] args) {
-        // Start here!
+        Scanner input = new Scanner(System.in);
+        int int1 = input.nextInt();
+        int int2 = input.nextInt();
+
+        double divide = (double) int1 / int2;
+        System.out.print(divide);
     }
 }
