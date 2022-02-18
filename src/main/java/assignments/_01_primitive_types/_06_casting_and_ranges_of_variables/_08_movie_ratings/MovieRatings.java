4ade1f0b9a019635a805db323b9028a815bc19f2
corock <hoongineer@gmail.com>
2022-02-18 09:17:56 +0900

    Add writing a program that asks the user for a movie rating by using casting

---
+++ src/main/java/assignments/_01_primitive_types/_06_casting_and_ranges_of_variables/_08_movie_ratings/MovieRatings.java
@@ -0,0 +1,8 @@
+package assignments._01_primitive_types._06_casting_and_ranges_of_variables._08_movie_ratings;
+
+public class MovieRatings {
+    public static void main(String[] args) {
+        // Start here!
+
+    }
+}
