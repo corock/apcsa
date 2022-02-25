9510bb71a06368261824f0577b66f3eeef5827ab
corock <hoongineer@gmail.com>
2022-02-25 18:06:54 +0900

    Update a program that is testing for underflow and overflow

--- src/main/java/assignments/_01_primitive_types/_06_casting_and_ranges_of_variables/_11_integer_overflow/IntegerOverflow.java
+++ src/main/java/assignments/_01_primitive_types/_06_casting_and_ranges_of_variables/_11_integer_overflow/IntegerOverflow.java
@@ -2,6 +2,9 @@ package assignments._01_primitive_types._06_casting_and_ranges_of_variables._11_
 
 public class IntegerOverflow {
     public static void main(String[] args) {
-        // Start here!
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE - 1);
+        System.out.println(Integer.MAX_VALUE + 1);
     }
 }
