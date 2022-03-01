1c96a38c1230d56c3db85812aa4f7996e3354344
corock <hoongineer@gmail.com>
2022-03-01 23:47:55 +0900

    Update instance variables in Pizza class

--- src/main/java/assignments/_02_using_objects/_01_instances_of_classes/_08_pizza_instance_variables/Pizza.java
+++ src/main/java/assignments/_02_using_objects/_01_instances_of_classes/_08_pizza_instance_variables/Pizza.java
@@ -1,5 +1,9 @@
 package assignments._02_using_objects._01_instances_of_classes._08_pizza_instance_variables;
 
 public class Pizza {
-    // Add your instance variables here
+    private String cheeseType;
+    private String topping;
+    private String crustType;
+    private int size;
+    private int cost;
 }
