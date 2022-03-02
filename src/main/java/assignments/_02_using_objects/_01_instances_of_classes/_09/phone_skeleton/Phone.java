2d7e68ae2c7777c6cb42b29226f4f5bf6891dc54
corock <hoongineer@gmail.com>
2022-03-02 22:27:25 +0900

    Add an another example for instance variables

---
+++ src/main/java/assignments/_02_using_objects/_01_instances_of_classes/_09/phone_skeleton/Phone.java
@@ -0,0 +1,10 @@
+package assignments._02_using_objects._01_instances_of_classes._09.phone_skeleton;
+
+/**
+ * The phone class represents a cellular phone.
+ * Add the instance variables you think a phone would need
+ */
+public class Phone {
+    // Attributes
+
+}
