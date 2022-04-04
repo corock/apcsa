2ceb91f2e8f81a0430aeedcbdac729b1b766c9d7
corock <hoongineer@gmail.com>
2022-04-04 23:55:11 +0900

    Add using the Rectangle class

---
+++ src/main/java/assignments/_02_using_objects/_02_creating_and_storing_objects/_06_using_the_rectangle_class/RectangleTester.java
@@ -0,0 +1,10 @@
+package assignments._02_using_objects._02_creating_and_storing_objects._06_using_the_rectangle_class;
+
+public class RectangleTester {
+    public static void main(String[] args) {
+        // Create a rectangle with width 5 and height 12
+
+        // Then print it out
+
+    }
+}
