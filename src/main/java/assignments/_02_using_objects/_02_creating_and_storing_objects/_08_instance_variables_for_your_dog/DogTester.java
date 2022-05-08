b8ccbb31c02274bb08266382fe655b14e11f77d1
corock <hoongineer@gmail.com>
2022-05-08 22:50:33 +0900

    Initialize adding an instance variable and including it as the second parameter to the constructor

---
+++ src/main/java/assignments/_02_using_objects/_02_creating_and_storing_objects/_08_instance_variables_for_your_dog/DogTester.java
@@ -0,0 +1,8 @@
+package assignments._02_using_objects._02_creating_and_storing_objects._08_instance_variables_for_your_dog;
+
+public class DogTester {
+    public static void main(String[] args) {
+        Dog golden = new Dog("Golden Retriever", "Sammy");
+        System.out.println(golden);
+    }
+}
