602e36ee4905f61321e551a3f91b7f0740e40e77
corock <hoongineer@gmail.com>
2022-07-16 23:54:02 +0900

    Add writing the class Pizza

---
+++ src/main/java/assignments/_02_using_objects/_02_creating_and_storing_objects/_09_pizza_time/Pizza.java
@@ -0,0 +1,17 @@
+package assignments._02_using_objects._02_creating_and_storing_objects._09_pizza_time;
+
+public class Pizza {
+
+    // Add the instance variables here
+
+
+    // Put the constructor here
+
+
+    // You don't need to do anything with this method
+    // Used to print the object
+    public String toString() {
+        return /* size + */ " inch " + /* type + */ " pizza with " /* + toppings */;
+    }
+
+}
---
+++ src/main/java/assignments/_02_using_objects/_02_creating_and_storing_objects/_09_pizza_time/PizzaTester.java
@@ -0,0 +1,10 @@
+package assignments._02_using_objects._02_creating_and_storing_objects._09_pizza_time;
+
+public class PizzaTester {
+
+    public static void main(String[] args) {
+        // Test your Pizza class here.
+
+    }
+
+}
