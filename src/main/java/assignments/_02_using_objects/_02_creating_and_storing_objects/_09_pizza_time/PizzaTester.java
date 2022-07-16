e4ff42832a075b03ecadf1da184e8f4850d75ce7
corock <hoongineer@gmail.com>
2022-07-17 00:03:55 +0900

    Update writing the class Pizza

--- src/main/java/assignments/_02_using_objects/_02_creating_and_storing_objects/_09_pizza_time/PizzaTester.java
+++ src/main/java/assignments/_02_using_objects/_02_creating_and_storing_objects/_09_pizza_time/PizzaTester.java
@@ -4,7 +4,14 @@ public class PizzaTester {
 
     public static void main(String[] args) {
         // Test your Pizza class here.
+        Pizza pineapple = new Pizza("pineapple", "pineapple and ham", 12);
+        System.out.println(pineapple);
 
+        Pizza kiwi = new Pizza("kiwi", "kiwi and sausage", 12);
+        System.out.println(kiwi);
+
+        Pizza apple = new Pizza("apple", "apple and chocolate", 12);
+        System.out.println(apple);
     }
 
 }
